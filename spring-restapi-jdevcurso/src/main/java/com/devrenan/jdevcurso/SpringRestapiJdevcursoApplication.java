a4e39c05d74ed6691ac1069e743f3e478680ffce
Thiago Renan <thiagorenan1108@gmail.com>
2026-01-16 23:05:58 -0300

    Projeto criado

---
+++ spring-restapi-jdevcurso/src/main/java/com/devrenan/jdevcurso/SpringRestapiJdevcursoApplication.java
@@ -0,0 +1,13 @@
+package com.devrenan.jdevcurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestapiJdevcursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestapiJdevcursoApplication.class, args);
+	}
+
+}
---
+++ spring-restapi-jdevcurso/src/test/java/com/devrenan/jdevcurso/SpringRestapiJdevcursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devrenan.jdevcurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestapiJdevcursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
