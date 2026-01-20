e9201a69ef00668e9964995423d44a5606beedc1
Thiago Renan <thiagorenan1108@gmail.com>
2026-01-19 21:40:37 -0300

    Criando classe de modelo - model/Usuario

---
+++ spring-restapi-jdevcurso/src/main/java/com/devrenan/jdevcurso/model/Usuario.java
@@ -0,0 +1,54 @@
+package com.devrenan.jdevcurso.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+
+@Entity
+public class Usuario implements Serializable{
+
+    private static final long seriaVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String nome;
+    private String login;
+    private int senha;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getSenha() {
+        return senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+}
