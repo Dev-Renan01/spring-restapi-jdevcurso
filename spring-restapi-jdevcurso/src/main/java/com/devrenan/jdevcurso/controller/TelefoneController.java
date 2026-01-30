c87ca58b5618185b3396f71af169480b24e325a7
Thiago Renan <thiagorenan1108@gmail.com>
2026-01-29 23:51:21 -0300

    Criando classe - TelefoneController

---
+++ spring-restapi-jdevcurso/src/main/java/com/devrenan/jdevcurso/controller/TelefoneController.java
@@ -0,0 +1,49 @@
+package com.devrenan.jdevcurso.controller;
+
+import com.devrenan.jdevcurso.model.Usuario;
+import com.devrenan.jdevcurso.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class TelefoneController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    
+    @PostMapping(value = "/salvar")
+    @ResponseStatus(HttpStatus.OK)
+    public Usuario salvar(@RequestBody Usuario usuario){
+        return usuarioService.salvar(usuario);
+    }
+
+    @GetMapping(value = "/listar")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Usuario> listar(){
+        return usuarioService.listarTodos();
+    }
+
+    @GetMapping(value = "/listarid/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Usuario> listarId(@PathVariable long id){
+        return usuarioService.listarId(id);
+    }
+
+    @PutMapping(value = "/atualizar")
+    @ResponseStatus(HttpStatus.OK)
+    public Usuario atualizar(@RequestBody Usuario usuario){
+        return usuarioService.atualizar(usuario);
+    }
+
+    @DeleteMapping(value = "/deletar")
+    @ResponseStatus(HttpStatus.OK)
+    public void deletarId(Long id){
+        usuarioService.deletar(id);
+    }
+}
+}
